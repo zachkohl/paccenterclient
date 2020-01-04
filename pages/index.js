843db0ced44875d66708659bc1d6dc39b8480062
zachkohl <zach.kohl@gmail.com>
2020-01-04 10:18:04 -0800

    fix react stuff

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,13 @@
 import ReactDOM from "react-dom";
 import React from "react";
-// import "../style.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
-import Value from "../components/value";
-import Add from "../components/add";
-import Subtract from "../components/subtract";
-import NewRole from "../components/newrole";
-import Header from "../components/Header";
-import DisplayRoles from "../components/DisplayRoles";
-import "../style.css";
 import Link from "next/link";
 
 const index = props => {
   return (
     <Provider store={store}>
-      <Header />
-      <NewRole />
-      <DisplayRoles />
-      <Link href="/backup">
-        <a>backup data</a>
-      </Link>
+      <p>welcome to PACCENTER</p>
     </Provider>
   );
 };
