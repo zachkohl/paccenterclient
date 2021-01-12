3c33dd5f1f9930b18c4a80a545a84f801cb9fcd9
thomasjzeman <thomasjzeman@gmail.com>
2021-01-11 21:32:04 -0800

    fixed signup page api

--- pages/api/newSignup.js
+++ pages/api/newSignup.js
@@ -1,56 +1,66 @@
 const db = require("../../lib/postgresSetup");
 
 export default async (req, res) => {
-//  try {
-//    const signupname = req.body.signupname;
-//    const signupemail = req.body.signupemail;
-//    const signupphone = req.body.signupphone;
- //   const signupaddress1 = req.body.signupaddress1;
-  //  const signupaddress2 = req.body.signupaddress2;
- //   const signupcity = req.body.signupcity;
-  //  const signupcounty = req.body.signupcounty;
- //   const signupstate = req.body.signupstate;
-  //  const signupzip = req.body.signupzip;
-//
- //   let text = "
-  //    INSERT INTO signup(
-   //     signup_name,
-    //    signup_email,
-  //      signup_phone,
-   //     signup_address_1,
-       // signup_address_2,
-     //   signup_city,
- //       signup_county,
- //       signup_state,
- //       signup_zip)
- //     VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)
- //     RETURNING *;";
+  try {
+    const signupname = req.body.signupname;
+    const signupemail = req.body.signupemail;
+    const signupphone = req.body.signupphone;
+    const signupaddress1 = req.body.signupaddress1;
+    const signupaddress2 = req.body.signupaddress2;
+    const signupcity = req.body.signupcity;
+    const signupcounty = req.body.signupcounty;
+    const signupstate = req.body.signupstate;
+    const signupzip = req.body.signupzip;
 
+console.log("console log one");
 
- //   let values = [
-  //    signupname,
-  //    signupemail, 
-  //    signupphone,
- //     signupaddress1,
-  //    signupaddress2,
-  //    signupcity,
-  //    signupcounty,
-  //    signupstate,
-  //    signupzip];
+    let text = `
+      INSERT INTO signup(
+      signup_name,
+      signup_email,
+      signup_phone,
+      signup_address_1,
+      signup_address_2,
+      signup_city,
+      signup_county,
+      signup_state,
+      signup_zip)
+      VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9)
+      RETURNING *;`
+      ;
 
+console.log("console log two");
 
-//  const getUser = await db.query(text, values);
- //       if (getUser === null) {
- //       res.send("fail");
- //     return;
- //   }
+    let values = [
+      signupname,
+      signupemail, 
+      signupphone,
+      signupaddress1,
+      signupaddress2,
+      signupcity,
+      signupcounty,
+      signupstate,
+      signupzip];
+      
+console.log("console log three");
 
- //   res.send("complete");
- // } catch (err) {
- //   console.log(err);
- //   res.send(err);
- // }
-console.log("function hit");
+      
+    const getUser = await db.query(text, values);
+      if (getUser === null) {
+      res.send("fail");
+      return;
+    }
+console.log("console log four");
+    res.send("complete");
+console.log("console log five");
+  }
+
+  catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+
+console.log("console log six");
 };
 
 
