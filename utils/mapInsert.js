e79595ee070d25173520dfe9cbc741ebc790c2eb
zachkohl <zach.kohl@gmail.com>
2021-01-18 19:43:03 -0800

    able to load polygons into database

--- utils/mapInsert.js
+++ utils/mapInsert.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
 const puppeteer = require('puppeteer');
+const axios = require('axios');
 
 async function readCSV(){
 
@@ -13,21 +14,40 @@ async function readCSV(){
     .on('error', error => console.error(error))
     .on('data', async row => {
         //process each row in the csv
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        const apiAddress = row.address;
-     await page.goto(apiAddress);
-      const content = await page.$('pre');
-      const text = await (await content.getProperty('textContent')).jsonValue();
-      blob = JSON.parse(text)
-console.log(blob)
-      let data = JSON.stringify(text);
-fs.writeFileSync('map.json', text);
-try{
-        let sql = `INSERT INTO MAPS(name,source,geom) VALUES($1,$2,ST_GeomFromGeoJSON('$3'))`;
-    let values = [row.name,row.address,blob.features[0].geometry];
+    //     const browser = await puppeteer.launch();
+    //     const page = await browser.newPage();
+    //     const apiAddress = row.address;
+    //  await page.goto(apiAddress);
+    //   const content = await page.$('pre');
+    //   const text = await (await content.getProperty('textContent')).jsonValue();
+    //   blob = JSON.parse(text)
 
-    const dbResponse = await db.query(sql, values);
+    try{
+   response = await axios.get(row.address)
+    blob = response.data;
+// console.log(blob)
+     let data = JSON.stringify(blob);
+
+       console.log(blob.features[0].geometry.rings[0])
+
+const latLong = blob.features[0].geometry.rings[0];
+
+let coordinates = '';
+for(let i=0;i<latLong.length;i++){
+    coordinates = coordinates + latLong[i][0] + ' ' + latLong[i][1]
+    if(i<latLong.length-1){
+        coordinates =  coordinates + ', '
+    }
+}
+
+//fs.writeFileSync('map.json', data);
+
+const testdata = `ST_GeogFromText('POLYGON((-116.049168634002 48.5179618100009, -117.032367779002 48.9768856540008 ,-116.049168634002 48.5179618100009))')`
+
+        let sql = `INSERT INTO MAPS(name,source,perimeter) VALUES('test2','need to learn to escape web addresses',ST_GeogFromText('POLYGON((${coordinates}))'));`;
+    let values = [];
+    fs.writeFileSync('map.txt', sql);
+  const dbResponse = await db.query(sql, values);
 }catch(error){
     console.log('database error',error)
 }
@@ -35,7 +55,7 @@ try{
 
 
 
-await browser.close();
+// await browser.close();
     })
     .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
 
--- utils/working.sql
+++ utils/working.sql
@@ -2,17 +2,18 @@
 //it is also incomplete, it is more like a working doc, hence the name.
 
 CREATE TABLE maps (
-    id uuid DEFAULT uuid_generate_v4 (),
+    uuid uuid DEFAULT uuid_generate_v4 (),
     name VARCHAR NOT NULL,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     source VARCHAR NOT NULL,
+    perimeter GEOGRAPHY(POLYGON),
     PRIMARY KEY (uuid)
 );
 
-SELECT AddGeometryColumn(
-  maps,
-  geom, 
-  4326,
-  polygon,
-  2
-);
+-- SELECT AddGeometryColumn(
+--   'maps',
+--   'geom', 
+--   3857,
+--   'polygon',
+--   2
+-- );
