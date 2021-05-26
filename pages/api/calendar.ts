b2bb3bd63c0322cb34c04668862fc72cbc071604
zachkohl <zach.kohl@gmail.com>
2021-05-25 19:39:43 -0700

    added minimal security

--- pages/api/calendar.ts
+++ pages/api/calendar.ts
@@ -4,11 +4,16 @@ export default async (req, res) => {
   const params = req.body;
 
   const address =
-    process.env.NODE_ENV === "production" ? "" : "http://localhost:5000/create";
+    process.env.NODE_ENV === "production"
+      ? "https://pythonpacapi.herokuapp.com/create"
+      : "http://localhost:5000/create";
 
   console.log(params);
 
-  const response = await axios.post(address, params);
+  const response = await axios.post(address, {
+    ...params,
+    key: process.env.pythonapi,
+  });
   console.log(response.data);
   res.send(response.data);
 };
